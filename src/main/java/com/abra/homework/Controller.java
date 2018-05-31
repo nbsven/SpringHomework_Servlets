f06425f82eb7dd8b6e36733b58c8f8b32dce8644
Nikolai Bystrov <isderevni@gmail.com>
2018-05-31 14:15:37 +0300

    done

--- src/main/java/com/abra/homework/Controller.java
+++ src/main/java/com/abra/homework/Controller.java
@@ -2,6 +2,8 @@ package com.abra.homework;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,33 +11,38 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Controller extends HttpServlet {
 
+  public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
   private static String state = "-1";
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("state_before = " + state);
-    state = "Get:" + LocalDate.now().toString();
+    state = "Get:" + LocalDateTime.now().format(FORMATTER);
     System.out.println("state_after = " + state);
+    resp.getWriter().append(state).close();
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("state_before = " + state);
-    state = "Post:" + LocalDate.now().toString();
+    state = "Post:" + LocalDateTime.now().format(FORMATTER);
     System.out.println("state_after = " + state);
+    resp.getWriter().append(state).close();
   }
 
   @Override
   protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("state_before = " + state);
-    state = "Put:" + LocalDate.now().toString();
+    state = "Put:" + LocalDateTime.now().format(FORMATTER);
     System.out.println("state_after = " + state);
+    resp.getWriter().append(state).close();
   }
 
   @Override
   protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     System.out.println("state_before = " + state);
-    state = "Delete:" + LocalDate.now().toString();
+    state = "Delete:" + LocalDateTime.now().format(FORMATTER);
     System.out.println("state_after = " + state);
+    resp.getWriter().append(state).close();
   }
 }
--- src/main/webapp/MyIndex.jsp
+++ src/main/webapp/MyIndex.jsp
@@ -9,9 +9,23 @@
     <title>Home</title>
 </head>
 <body>
-    <form action="/controller" method="post">
-        <input type="submit" name="Form Button">
-    </form>
+<p id="demo">no content yet</p>
+<button type="button" id="button_get" name="GET" onclick="doRequest('GET')">Get</button>
+<button type="button" id="button_post" name="POST" onclick="doRequest('POST')">Post</button>
+<button type="button" id="button_put" name="PUT" onclick="doRequest('PUT')">Put</button>
+<button type="button" id="button_delete" name="DELETE" onclick="doRequest('DELETE')">Delete</button>
 </body>
 
+<script>
+  function doRequest(request) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        document.getElementById("demo").innerHTML=this.responseText;
+      }
+    };
+    xhttp.open(request, "/controller", true);
+    xhttp.send();
+  }
+</script>
 </html>
